86f05cb1e5b764199e3ed164ee6a97645b5b2897
alexdeathway <alexdeathway@gmail.com>
2022-01-04 17:28:13 +0530

    added resources and statics

--- twws/urls.py
+++ twws/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from users.views import UserSignupView
 from .views import Home
@@ -38,3 +40,6 @@ urlpatterns = [
     path('reset-password-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
     path('reset-password-complete/',PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
